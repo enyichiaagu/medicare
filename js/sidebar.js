38ca72da5526c7fa9e66b5f8a320d75e9a4da77f
enyichiaagu <enyichiaagu@gmail.com>
2023-07-07 12:38:37 +0100

    Finished Sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,26 +1,19 @@
-const dropDownOptions = document.querySelectorAll('nav ul.navigation > li')
-
-const dropDownsToArray = Array.from(dropDownOptions)
-
-const filteredOptions = dropDownsToArray.filter((option) =>
-	option.querySelector('.down-arrow')
+const dropDownIcons = document.querySelectorAll(
+	'nav ul.navigation > li .down-arrow'
 )
 
-filteredOptions.forEach((element) => {
-	element.addEventListener('click', toggleSubMenu)
+const dropDownsToArray = Array.from(dropDownIcons)
+
+dropDownsToArray.forEach((element, index) => {
+	element.parentElement.addEventListener('click', ({ currentTarget }) =>
+		toggleSubMenu(currentTarget, index)
+	)
 })
 
-function toggleSubMenu({ currentTarget }) {
-	const subMenuBlock = currentTarget.querySelector('.sub-menu-items')
-	subMenuBlock.classList.toggle('sub-menu-open')
+function toggleSubMenu(currentTarget, index) {
+	currentTarget.nextElementSibling.classList.toggle('sub-menu-open')
+	dropDownIcons[index].textContent =
+		dropDownIcons[index].textContent === 'arrow_drop_down'
+			? 'arrow_drop_up'
+			: 'arrow_drop_down'
 }
-
-// const ButtonsToArray = Array.from(dropDownButtons)
-
-// const optionsArray = ButtonsToArray.map(
-// 	(option) => option.parentElement.parentElement
-// )
-
-// console.log(dropDownOptions[0].parentNode)
-// console.log(dropDownOptions[1].parentNode)
-// console.log(dropDownOptions[2].parentNode)
